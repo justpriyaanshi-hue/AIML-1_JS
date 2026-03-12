118dff7d4b112ed6bdaf4ebaa0fd2ebfdb7662da
Priyanshi Singh <justpriyaanshi@gmail.com>
2026-03-12 22:41:17 +0530

    description

---
+++ 2.js
@@ -0,0 +1,16 @@
+let collection = [
+    {},
+    [],
+    true,
+    "john",
+    function (){},
+    9900283797912,
+    undefined,
+    null,
+    new string ("abc"),
+    new Date(),
+
+];
+console.log(collection);
+collection.teacherName = "John Mac";
+collection.phoneno = 8963986899348;
---
+++ script.js
@@ -0,0 +1,30 @@
+/* function add(a,b){
+    console.log(a+b);
+}add(10,20);
+var add2 = (a,b) => {
+    console.log(a+b);
+};
+add2(102,20); */
+
+<script type="text/javascript">
+var person = {
+    name : John mac,
+    age: 30,
+    isMarried : true,
+    address: {
+        street : "vijay nagar",
+        flatNO: 201
+    }
+};
+console.log(person);
+
+var person2 = new Object();
+person2.name="krishnkant";
+person2.name 
+
+
+
+
+
+</>
+
